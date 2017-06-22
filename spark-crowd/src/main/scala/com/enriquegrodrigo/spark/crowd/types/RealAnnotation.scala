45aaabb8731e0e8ffacd58c159f5a3706144b171
Enrique <enrikgr.93@gmail.com>
2017-06-22 09:55:18 +0200

    Adds RealAnnotation and RealLabel

---
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/RealAnnotation.scala
@@ -0,0 +1,4 @@
+package com.enriquegrodrigo.spark.crowd.types
+
+case class RealAnnotation(example: Long, annotator: Long, value: Double) 
+
---
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/RealLabel.scala
@@ -0,0 +1,5 @@
+
+package com.enriquegrodrigo.spark.crowd.types
+
+case class RealLabel(example: Long, value: Double) 
+
