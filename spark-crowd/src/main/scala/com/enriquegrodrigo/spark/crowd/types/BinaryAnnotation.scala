2fba9ea9a690923f73130bae2de6c4bf9c29cf79
Enrique <enrikgr.93@gmail.com>
2017-06-23 12:35:06 +0200

    Adds documentation for annotation classes

--- spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/BinaryAnnotation.scala
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/BinaryAnnotation.scala
@@ -1,4 +1,13 @@
 
 package com.enriquegrodrigo.spark.crowd.types
 
+/**
+ * class for storing a binary annotation.
+ *
+ * @param example example for which the annotation is made
+ * @param annotator annotator that made the annotation
+ * @param value value of the annotation 
+ * @author enrique.grodrigo
+ * @version 0.1
+ */
 case class BinaryAnnotation(example: Long, annotator: Long, value: Int)
--- spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/RealAnnotation.scala
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/RealAnnotation.scala
@@ -1,4 +1,13 @@
 package com.enriquegrodrigo.spark.crowd.types
 
+/**
+ * Class for storing real annotations.
+ *
+ * @param example example for which the annotation is made
+ * @param annotator annotator that made the annotation
+ * @param value value of the annotation 
+ * @author enrique.grodrigo
+ * @version 0.1
+ */
 case class RealAnnotation(example: Long, annotator: Long, value: Double) 
 
