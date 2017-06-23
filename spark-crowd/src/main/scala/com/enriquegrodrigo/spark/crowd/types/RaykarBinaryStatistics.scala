c19ad2c8998e111ed0b723d6f9381e3a256782a0
Enrique <enrikgr.93@gmail.com>
2017-06-23 13:23:42 +0200

    Documents RaykarBinaryStatistics

--- spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/RaykarBinaryStatistics.scala
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/RaykarBinaryStatistics.scala
@@ -4,5 +4,14 @@ package com.enriquegrodrigo.spark.crowd.types
 import org.apache.spark.sql.Dataset
 import org.apache.spark.broadcast.Broadcast
 
+/**
+ * Class for storing the RaykarBinary annotation statistics needed for the learning process [[com.enriquegrodrigo.spark.crowd.methods.RaykarBinary]].
+ *
+ * @param example example to which the information refers 
+ * @param a a statistic 
+ * @param b b statistic 
+ * @author enrique.grodrigo
+ * @version 0.1
+ */
 private[crowd] case class RaykarBinaryStatistics(example: Long, a: Double, b: Double)
 
