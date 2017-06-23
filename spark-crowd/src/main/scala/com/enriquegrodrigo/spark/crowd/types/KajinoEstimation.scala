22f363c8d4688c11e110aa299b0714c8c66dbbaf
Enrique <enrikgr.93@gmail.com>
2017-06-23 13:23:19 +0200

    Documents KajinoEstimation

--- spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/KajinoEstimation.scala
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/KajinoEstimation.scala
@@ -1,6 +1,13 @@
 package com.enriquegrodrigo.spark.crowd.types
 
-
+/**
+ * Class for storing the ground thruth estimation of the Kajino method [[com.enriquegrodrigo.spark.crowd.methods.Kajino]].
+ *
+ * @param example example to which the information refers 
+ * @param est estimation of the ground truth label 
+ * @author enrique.grodrigo
+ * @version 0.1
+ */
 private[crowd] case class KajinoEstimation(example: Long, est: Double)
 
 
