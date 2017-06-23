76fc186e3e6c1ba588ae70dae78daad63ef4cdf1
Enrique <enrikgr.93@gmail.com>
2017-06-23 13:21:37 +0200

    Documents Params classes

--- spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/DawidSkeneParams.scala
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/DawidSkeneParams.scala
@@ -4,5 +4,13 @@ package com.enriquegrodrigo.spark.crowd.types
 import org.apache.spark.sql.Dataset
 import org.apache.spark.broadcast.Broadcast
 
+/**
+ * Class for storing the DawidSkene method params [[com.enriquegrodrigo.spark.crowd.methods.DawidSkene]].
+ *
+ * @param pi confusion matrix for each annotator 
+ * @param w weight for each class
+ * @author enrique.grodrigo
+ * @version 0.1
+ */
 case class DawidSkeneParams(pi: Array[Array[Array[Double]]], w: Array[Double])
 
