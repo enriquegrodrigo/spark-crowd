23a8357d6d1b84f2d9f809cebe62c58b9647de1d
Enrique <enrikgr.93@gmail.com>
2017-06-23 13:22:52 +0200

    Documents partial classes

--- spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/GladPartial.scala
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/GladPartial.scala
@@ -1,4 +1,15 @@
 
 package com.enriquegrodrigo.spark.crowd.types
 
+/**
+ * Class for storing the Glad method partial annotation information [[com.enriquegrodrigo.spark.crowd.methods.DawidSkene]].
+ *
+ * @param example example to which the information refers 
+ * @param annotator annotator to which the record refers
+ * @param value value of the annotation 
+ * @param est estimation of the ground truth label 
+ * @param beta difficulty of the example 
+ * @author enrique.grodrigo
+ * @version 0.1
+ */
 private[crowd] case class GladPartial(example: Long, annotator: Int, value: Int, est: Double, beta: Double)
--- spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/RaykarBinaryPartial.scala
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/RaykarBinaryPartial.scala
@@ -1,4 +1,13 @@
 
 package com.enriquegrodrigo.spark.crowd.types
-
+/**
+ * Class for storing the RaykarBinary method partial annotation information [[com.enriquegrodrigo.spark.crowd.methods.RaykarBinary]].
+ *
+ * @param example example to which the information refers 
+ * @param annotator annotator to which the record refers
+ * @param value value of the annotation 
+ * @param mu estimation of the ground truth label 
+ * @author enrique.grodrigo
+ * @version 0.1
+ */
 private[crowd] case class RaykarBinaryPartial(example: Long, annotator: Int, value: Int, mu: Double)
