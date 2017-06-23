0bb782d602524631ca87ccde179e3a2aa51f9716
Enrique <enrikgr.93@gmail.com>
2017-06-23 13:26:16 +0200

    Removes unused RaykarBinaryExample

--- spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/RaykarBinaryExample.scala
+++
@@ -1,7 +0,0 @@
-
-package com.enriquegrodrigo.spark.crowd.types
-
-import org.apache.spark.sql.Dataset
-import org.apache.spark.broadcast.Broadcast
-
-private[crowd] case class RaykarBinaryExample(example: Long, mu: Double, featureVector: Array[Double], a: Double, b: Double, p: Double)
