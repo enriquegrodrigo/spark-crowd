a84c499b11765ee5faa00a16caa36549557523d2
Enrique G. Rodrigo <Enrique.GRodrigo@gmail.com>
2018-06-14 11:48:30 +0200

    Adds CGlad types

---
+++ spark-crowd/src/main/scala/com/enriquegrodrigo/spark/crowd/types/CGladModel.scala
@@ -0,0 +1,85 @@
+/*
+ * MIT License 
+ *
+ * Copyright (c) 2017 Enrique González Rodrigo 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+ * persons to whom the Software is furnished to do so, subject to the following conditions: 
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.enriquegrodrigo.spark.crowd.types
+
+import org.apache.spark.sql.Dataset
+import org.apache.spark.broadcast.Broadcast
+
+/**
+*  Glad model returned by the Glad method 
+*
+*  @param mu label estimation returned from the model.
+*  @param alphas precision of annotator given by the Glad model.
+*  @param betas instance difficulty given by Glad model. 
+*  @param logLikelihood logLikelihood of the final estimation of the model.  
+*  @author enrique.grodrigo
+*  @version 0.1 
+*/
+class CGladModel(mu: Dataset[BinarySoftLabel], 
+                          prec: Array[Double], 
+                          diffic: Dataset[GladInstanceDifficulty],
+                          ranks: Dataset[ExampleRanks],
+                          clusters: Dataset[ExampleCluster]) extends Model[BinarySoftLabel] {
+                            
+  /**
+  *  Method that returns the probabilistic estimation of the true label 
+  *
+  *  @return org.apache.spark.sql.Dataset
+  *  @author enrique.grodrigo
+  *  @version 0.1 
+  */
+  def getMu(): Dataset[BinarySoftLabel] = mu 
+
+  /**
+  *  Method that returns the annotator precision information 
+  *
+  *  @return Double 
+  *  @author enrique.grodrigo
+  *  @version 0.1 
+  */
+  def getAnnotatorPrecision(): Array[Double] = prec 
+
+  /**
+  *  Method that returns information about instance difficulty
+  *
+  *  @return Double 
+  *  @author enrique.grodrigo
+  *  @version 0.1 
+  */
+  def getInstanceDifficulty(): Dataset[GladInstanceDifficulty] = diffic 
+
+  /**
+  *  Method that returns information about ranks  
+  *
+  *  @return Double 
+  *  @author enrique.grodrigo
+  *  @version 0.1 
+  */
+  def getRankData(): Dataset[ExampleRanks] = ranks 
+
+  /**
+  *  Method that returns information about clusters 
+  *
+  *  @return Double 
+  *  @author enrique.grodrigo
+  *  @version 0.1 
+  */
+  def getClusters(): Dataset[ExampleCluster]= clusters 
+}
